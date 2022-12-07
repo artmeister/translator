db6ba34ef468d31731b8c736ddf4caf77ce7d087
artmeister <artiom.mamadaliev@yandex.by>
2022-12-07 13:43:53 +0300

    fix

--- src/components/HistoryTranslate/HistoryItem/index.jsx
+++ src/components/HistoryTranslate/HistoryItem/index.jsx
@@ -1,23 +1,12 @@
 const HistoryItem = ({ index, langPair, inputText, resultText }) => {
 	return (
     <tr>
-      <td scope="row">{index}</td>
+      <td>{index}</td>
       <td>{langPair}</td>
       <td>{inputText}</td>
       <td>{resultText}</td>
     </tr>
 	)
 }
-
-// const HistoryItem = (index, langPair, inputText, resultText) => {
-// 	return (
-    // <tr>
-    //   <td scope="row">{index}</td>
-    //   <td>{langPair}</td>
-    //   <td>{inputText}</td>
-    //   <td>{resultText}</td>
-    // </tr>
-// 	)
-// }
 
 export default HistoryItem;
--- src/components/TextTranslate/index.jsx
+++ src/components/TextTranslate/index.jsx
@@ -7,15 +7,15 @@ import { add } from '../../redux/historySlice';
 
 const TextTranslate = () => {
 	const [inputText, setInputText] = useState('');
-	const [selectedLanguageKeyFrom, setLanguageKeyFrom] = useState('');
-	const [selectedLanguageKeyTo, setLanguageKeyTo] = useState('');
+	const [selectedLanguageKeyFrom, setLanguageKeyFrom] = useState('ru');
+	const [selectedLanguageKeyTo, setLanguageKeyTo] = useState('en');
 	const [resultText, setResultText] = useState('');
 
 	const dispatch = useDispatch();
 
 	const langPair = `${selectedLanguageKeyFrom}|${selectedLanguageKeyTo}`;
 	const url = `https://api.mymemory.translated.net/get?q=${inputText}&langpair=${langPair}`;
-	const regex = /(empty|^$)/;
+	// const regex = /(empty|^$)/;
 
 	const languageKeyFrom = (selectedLanguage) => {
 		setLanguageKeyFrom(selectedLanguage.target.value);
@@ -26,29 +26,33 @@ const TextTranslate = () => {
 
 	const translateText = async () => {
 		if (inputText.length >= 2) {
-			if (
-				!selectedLanguageKeyFrom.match(regex) &&
-				!selectedLanguageKeyTo.match(regex)
-			) {
+			// if (
+			// 	!selectedLanguageKeyFrom.match(regex) &&
+			// 	!selectedLanguageKeyTo.match(regex)
+			// ) {
 				await axios.post(url)
 					.then(async (response) => {
 						if (response.data.responseData.match) setResultText(response.data.responseData.translatedText)
-							else console.log(response.data.responseData.translatedText);
+							else alert(response.data.responseData.translatedText)
 					})
-			} else alert('Выберите языки');
-		} else alert('Введите текст');
+					.catch(error => {
+						alert(error)
+					})
+			// } else alert('Выберите языки');
+		} else alert('Введите больше текста');
  	}
 
 	useEffect(() => {
 		if (resultText !== '') dispatch(add({ inputText, resultText, langPair }))
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [resultText])
 
 	return (
 		<>
 			<h1>React Translate App</h1>
 			<div className="mb-3">
-				<select defaultValue="empty" className="form-select" aria-label="Default select example" onChange={languageKeyFrom}>
-					<option value="empty">Select Language</option>
+				<select defaultValue="ru" className="form-select" aria-label="Default select example" onChange={languageKeyFrom}>
+					{/* <option value="empty">Select Language</option> */}
 					<option value="ru">Russian</option>
 					<option value="en">English</option>
 					<option value="de">Deutch</option>
@@ -64,8 +68,8 @@ const TextTranslate = () => {
 			<button type="button" className="btn btn-primary mb-4" onClick={translateText}>Translate</button>
 
 			<div className="mb-3">
-				<select defaultValue="empty" className="form-select" aria-label="Default select example" onChange={languageKeyTo}>
-					<option value="empty">Select Language</option>
+				<select defaultValue="en" className="form-select" aria-label="Default select example" onChange={languageKeyTo}>
+					{/* <option value="empty">Select Language</option> */}
 					<option value="ru">Russian</option>
 					<option value="en">English</option>
 					<option value="de">Deutch</option>
